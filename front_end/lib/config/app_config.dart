3d29790e57d7d86a0ac8411dc7fc930688349c31
Idriss NT <ngomseuidris10@gmail.com>
2025-01-21 22:37:54 +0100

    Remove log files and database from git tracking

--- front_end/lib/config/app_config.dart
+++ front_end/lib/config/app_config.dart
@@ -6,8 +6,8 @@ class AppConfig {
   // API Configuration
   static const String baseUrl = isProduction
       ? 'https://yourdomain.com/api/auth' // Replace with your production URL
-      // : 'http://172.20.10.10:8000/api/auth';
-      : 'http://10.0.2.2:8000/api/auth';
+      : 'http://172.20.10.10:8000/api/auth'; // url local windows. help to connect phisical device
+  // : 'http://10.0.2.2:8000/api/auth'; // url emulator android
 
   // Logging Configuration
   static const bool enableLogging = !isProduction;
